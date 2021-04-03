51b37c82b146943a121b86bf8b28aa0e3f950a82
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-03 14:47:07 -0300

    Create a new category

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import { ICategoriesRepository } from '@modules/cars/repositories/contracts/ICategoriesRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+@injectable()
+class CreateCategoryUseCase {
+  constructor(
+    @inject('CategoriesRepository')
+    private categoriesRepository: ICategoriesRepository
+  ) {}
+
+  async execute({ name, description }: IRequest): Promise<void> {
+    if (await this.categoriesRepository.findByName(name)) {
+      throw new AppError('Category already exists');
+    }
+
+    await this.categoriesRepository.create({ name, description });
+  }
+}
+
+export default CreateCategoryUseCase;
