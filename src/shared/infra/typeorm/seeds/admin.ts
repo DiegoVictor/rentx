b604f055cee445b62f534c8e3d47d21baf0008d7
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-03 14:44:48 -0300

    Create admin user

---
+++ src/shared/infra/typeorm/seeds/admin.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { v4 as uuidV4 } from 'uuid';
+import { hash } from 'bcrypt';
+import createConnection from '../index';
+
+async function create() {
+  const connection = await createConnection(process.env.DB_HOST);
+
+  const id = uuidV4();
+  const password = await hash('admin', 8);
+
+  await connection.query(
+    `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+    values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', '7623487234')`
+  );
+
+  await connection.close();
+}
+
+create().then(() => console.log('Admin user created'));
