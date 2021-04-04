a0c7fab25d1a544c7ab30290e18dcc6788412dce
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-04 13:06:45 -0300

    Create in memory implementation

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,30 @@
+import Category from '@modules/cars/infra/typeorm/entities/Category';
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from '../contracts/ICategoriesRepository';
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    return this.categories.find((category) => category.name === name);
+  }
+
+  async list(): Promise<Category[]> {
+    return this.categories;
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export default CategoriesRepositoryInMemory;
