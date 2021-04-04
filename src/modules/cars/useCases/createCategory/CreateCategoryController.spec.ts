7906833f8c0fc721e4a0dca5313b9b71e78e5441
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-04 13:07:40 -0300

    Test category creation controller

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,80 @@
+import request from 'supertest';
+import { Connection, createConnection } from 'typeorm';
+import { hash } from 'bcrypt';
+import { v4 as uuidV4 } from 'uuid';
+
+import app from '@shared/infra/http/app';
+
+describe('Create Category Controller', () => {
+  const email = 'admin@rentx.com.br';
+  let password = 'admin';
+  let connection: Connection;
+
+  beforeAll(async () => {
+    connection = await createConnection();
+
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const passwordHash = await hash(password, 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+      values('${id}', 'admin', '${email}', '${passwordHash}', true, 'now()', '7623487234')`
+    );
+  });
+
+  beforeEach(async () => {
+    await connection.query('DELETE FROM categories');
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to create a new category', async () => {
+    const {
+      body: { token },
+    } = await request(app).post('/v1/sessions').send({
+      email,
+      password,
+    });
+
+    await request(app)
+      .post('/v1/categories')
+      .set({ Authorization: `Bearer ${token}` })
+      .expect(201)
+      .send({
+        name: 'Category A',
+        description: 'Lorem Ipsum Dolor Sit Amet',
+      });
+  });
+
+  it('should not be able to create a new category with duplicated name', async () => {
+    const {
+      body: { token },
+    } = await request(app).post('/v1/sessions').send({
+      email,
+      password,
+    });
+
+    await request(app)
+      .post('/v1/categories')
+      .set({ Authorization: `Bearer ${token}` })
+      .expect(201)
+      .send({
+        name: 'Category A',
+        description: 'Lorem Ipsum Dolor Sit Amet',
+      });
+
+    await request(app)
+      .post('/v1/categories')
+      .set({ Authorization: `Bearer ${token}` })
+      .expect(400)
+      .send({
+        name: 'Category A',
+        description: 'Lorem Ipsum Dolor Sit Amet',
+      });
+  });
+});
