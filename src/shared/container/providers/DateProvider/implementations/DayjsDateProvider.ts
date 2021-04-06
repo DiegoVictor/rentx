de36bfec92061524f79d6f741fe8ad73634e2129
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-06 17:28:57 -0300

    Implement date provider

---
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -0,0 +1,26 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+
+import IDateProvider from '../contracts/IDateProvider';
+
+dayjs.extend(utc);
+
+class DayjsDateProvider implements IDateProvider {
+  isBefore(date: Date, compare_date: Date): boolean {
+    return dayjs(this.toUTC(date)).isBefore(this.toUTC(compare_date));
+  }
+
+  toUTC(date: Date): Date {
+    return dayjs(date).utc().local().toDate();
+  }
+
+  add(date: Date, number: number, unit: dayjs.OpUnitType): Date {
+    return dayjs(this.toUTC(date)).add(number, unit).toDate();
+  }
+
+  diffInDays(start_date: Date, end_date: Date): number {
+    return dayjs(this.toUTC(start_date)).diff(this.toUTC(end_date), 'days');
+  }
+}
+
+export default DayjsDateProvider;
