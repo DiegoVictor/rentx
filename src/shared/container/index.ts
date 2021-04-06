e0b0cf538ddf1ce35cecabbc9f4e4f34b92faf85
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-06 17:31:26 -0300

    Register user repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,18 @@
 import { container } from 'tsyringe';
 
+import '@shared/container/providers';
+
+import IUsersRepository from '@modules/accounts/repositories/contracts/IUsersRepository';
+import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/contracts/ICategoriesRepository';
 import CategoriesRepository from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
   CategoriesRepository
+);
+
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository
 );
