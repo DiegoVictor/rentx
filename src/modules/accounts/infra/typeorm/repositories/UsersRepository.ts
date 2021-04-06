d5cbc7bb29ff76626c8ab310e7c867aae94e3956
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-06 17:27:17 -0300

    Implement users repository

---
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,45 @@
+import { getRepository, Repository } from 'typeorm';
+
+import ICreateUserDTO from '@modules/accounts/dtos/ICreateUserDTO';
+import User from '@modules/accounts/infra/typeorm/entities/User';
+import IUsersRepository from '../../../repositories/contracts/IUsersRepository';
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create({
+    name,
+    email,
+    driver_license,
+    password,
+    id,
+    avatar,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      email,
+      driver_license,
+      password,
+      id,
+      avatar,
+    });
+
+    await this.repository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
+    return user;
+  }
+}
+
+export default UsersRepository;
