601b11ad33e79585c002667c12cb08300e720c01
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-07 18:12:22 -0300

    Create database connection

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
+import createConnection from '@shared/infra/typeorm';
+createConnection(process.env.DB_HOST);
 const app = express();
 export default app;
