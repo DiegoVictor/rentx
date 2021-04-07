49e3a3ddb58c3a03c5d5c08a0f76056e7b3d3f20
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-07 18:12:34 -0300

    Allow json payloads

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,4 +4,5 @@ import express, { NextFunction, Request, Response } from 'express';
 import createConnection from '@shared/infra/typeorm';
 createConnection(process.env.DB_HOST);
 const app = express();
+app.use(express.json());
 export default app;
