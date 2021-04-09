6f07945e0dd5fb648edb25aaa5d243dab4346758
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-09 18:07:08 -0300

    Configure routes

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,10 +4,14 @@ import 'express-async-errors';
 import express, { NextFunction, Request, Response } from 'express';
 import '@shared/container';
 import createConnection from '@shared/infra/typeorm';
+import routes from '@shared/infra/http/routes';
 import AppError from '@shared/errors/AppError';
 createConnection(process.env.DB_HOST);
 const app = express();
 app.use(express.json());
+
+app.use('/v1', routes);
+
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
