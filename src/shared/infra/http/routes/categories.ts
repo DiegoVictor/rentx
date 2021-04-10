13061404a240703968c620617bdee20e1aba108a
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-10 16:02:11 -0300

    Create route to import categories

--- src/shared/infra/http/routes/categories.ts
+++ src/shared/infra/http/routes/categories.ts
@@ -2,15 +2,18 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import CreateCategoryController from '@modules/cars/useCases/createCategory/CreateCategoryController';
+import ImportCategoryController from '@modules/cars/useCases/importCategory/ImportCategoryController';
 import ListCategoriesController from '@modules/cars/useCases/listCategories/ListCategoriesController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import ensureAdmin from '../middlewares/ensureAdmin';
+
 const app = Router();
 const upload = multer({
   dest: './tmp',
 });
 
 const createCategoryController = new CreateCategoryController();
+const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
 app.get('/', listCategoriesController.handle);
@@ -19,6 +22,14 @@ app.post(
   ensureAuthenticated,
   ensureAdmin,
   createCategoryController.handle
+);
+
+app.post(
+  '/import',
+  upload.single('file'),
+  ensureAuthenticated,
+  ensureAdmin,
+  importCategoryController.handle
 );
 
 export default app;
