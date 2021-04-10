c390f1755e916bc8304dccf2295ebdf7591f369f
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-10 16:01:57 -0300

    Configure multer middleware

--- src/shared/infra/http/routes/categories.ts
+++ src/shared/infra/http/routes/categories.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import CreateCategoryController from '@modules/cars/useCases/createCategory/CreateCategoryController';
 import ListCategoriesController from '@modules/cars/useCases/listCategories/ListCategoriesController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import ensureAdmin from '../middlewares/ensureAdmin';
 const app = Router();
+const upload = multer({
+  dest: './tmp',
+});
+
 const createCategoryController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
