db50d380a7d40214c4f94e6fa9233058ce6d3de8
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-10 16:00:21 -0300

    Create list route

--- src/shared/infra/http/routes/categories.ts
+++ src/shared/infra/http/routes/categories.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import CreateCategoryController from '@modules/cars/useCases/createCategory/CreateCategoryController';
+import ListCategoriesController from '@modules/cars/useCases/listCategories/ListCategoriesController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import ensureAdmin from '../middlewares/ensureAdmin';
 const app = Router();
 const createCategoryController = new CreateCategoryController();
+const listCategoriesController = new ListCategoriesController();
+
+app.get('/', listCategoriesController.handle);
 app.post(
   '/',
   ensureAuthenticated,
