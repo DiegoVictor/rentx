08beb6ca8400196cb0b6c104513b8d4012925952
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-11 15:27:22 -0300

    Create controller to set user avatar

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import UpdateUserAvatarUseCase from './UpdateUserAvatarUseCase';
+
+class UpdateUserAvatarController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.user;
+    const { filename } = request.file;
+
+    const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
+    updateUserAvatarUseCase.execute({ user_id: id, avatar_file: filename });
+
+    return response.sendStatus(204);
+  }
+}
+
+export default UpdateUserAvatarController;
