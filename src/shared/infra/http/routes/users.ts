3011b2dfe9fc4218e9fccd285d2788186baa24d8
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-11 15:26:10 -0300

    Create route to add new users

--- src/shared/infra/http/routes/users.ts
+++ src/shared/infra/http/routes/users.ts
@@ -1,3 +1,6 @@
 import { Router } from 'express';
+import CreateUserController from '@modules/accounts/useCases/createUser/CreateUserController';
 const app = Router();
+const createUserController = new CreateUserController();
+app.post('/', createUserController.handle);
 export default app;
