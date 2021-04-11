a7e0cac6334891c1594fc7ff00e11551813ede49
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-11 15:28:25 -0300

    Configure route to set user avatar

--- src/shared/infra/http/routes/users.ts
+++ src/shared/infra/http/routes/users.ts
@@ -1,10 +1,23 @@
 import { Router } from 'express';
 import multer from 'multer';
+
 import CreateUserController from '@modules/accounts/useCases/createUser/CreateUserController';
+import UpdateUserAvatarController from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 import uploadConfig from '@config/upload';
+import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
+
 const app = Router();
 const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
+
 app.post('/', createUserController.handle);
+app.patch(
+  '/avatar',
+  ensureAuthenticated,
+  uploadAvatar.single('avatar'),
+  updateUserAvatarController.handle
+);
+
 export default app;
