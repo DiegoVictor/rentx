80e5eec89d7fe41a4e7ed51c6f9f1bca8b24e2c1
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-11 15:25:16 -0300

    Create user creation uase case

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,37 @@
+import { hash } from 'bcrypt';
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '@modules/accounts/repositories/contracts/IUsersRepository';
+import ICreateUserDTO from '@modules/accounts/dtos/ICreateUserDTO';
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    email,
+    driver_license,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const hashedPassword = await hash(password, 8);
+
+    const user = await this.usersRepository.findByEmail(email);
+    if (user) {
+      throw new AppError('User already exists');
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      driver_license,
+      password: hashedPassword,
+    });
+  }
+}
+
+export default CreateUserUseCase;
