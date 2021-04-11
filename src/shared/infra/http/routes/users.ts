3ee6f328a7e8bd63b472b32e53beef383a340586
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-11 15:28:03 -0300

    Configure upload middleware

--- src/shared/infra/http/routes/users.ts
+++ src/shared/infra/http/routes/users.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserController from '@modules/accounts/useCases/createUser/CreateUserController';
+import uploadConfig from '@config/upload';
 const app = Router();
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+
 const createUserController = new CreateUserController();
 app.post('/', createUserController.handle);
 export default app;
