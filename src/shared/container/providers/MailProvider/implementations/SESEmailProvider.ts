d70bcf72b1e11d926f65a9810ea34906f6f06ec2
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-12 19:15:44 -0300

    Create SES implementation

---
+++ src/shared/container/providers/MailProvider/implementations/SESEmailProvider.ts
@@ -0,0 +1,40 @@
+import { injectable } from 'tsyringe';
+import { SES } from 'aws-sdk';
+import nodemailer, { Transporter } from 'nodemailer';
+import fs from 'fs';
+import handlebars from 'handlebars';
+
+import IMailProvider from '../contracts/IMailProvider';
+
+@injectable()
+class SESEmailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    this.client = nodemailer.createTransport({
+      SES: new SES({
+        apiVersion: '2010-12-01',
+        region: process.env.AWS_REGION,
+      }),
+    });
+  }
+
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void> {
+    const template = fs.readFileSync(path).toString('utf-8');
+    const parser = handlebars.compile(template);
+
+    await this.client.sendMail({
+      to,
+      from: 'Rentx <noreply@rentx.com.br>',
+      subject,
+      html: parser(variables),
+    });
+  }
+}
+
+export default SESEmailProvider;
