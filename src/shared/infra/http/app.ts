505df618bf215e7bfea3343260e0af98c0b7361d
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-13 19:42:34 -0300

    Serve static files

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -9,6 +9,8 @@ import AppError from '@shared/errors/AppError';
 createConnection(process.env.DB_HOST);
 const app = express();
 app.use(express.json());
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use('/v1', routes);
 
