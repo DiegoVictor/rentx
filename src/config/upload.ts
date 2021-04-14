09ab24c406289ba9ee615bcc7e764db0e03d0fe7
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-14 17:43:33 -0300

    Update upload configuration

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,18 +2,17 @@ import multer from 'multer';
 import { resolve } from 'path';
 import crypto from 'crypto';
 
-export default {
-  upload(folder: string) {
-    return {
-      storage: multer.diskStorage({
-        destination: resolve(__dirname, '..', '..', folder),
-        filename: (request, file, callback) => {
-          const hash = crypto.randomBytes(16).toString('hex');
-          const fileName = `${hash}-${file.originalname}`;
+const tmpFolder = resolve(__dirname, '..', '..', 'tmp');
 
-          callback(null, fileName);
-        },
-      }),
-    };
-  },
+export default {
+  tmpFolder,
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename: (request, file, callback) => {
+      const hash = crypto.randomBytes(16).toString('hex');
+      const fileName = `${hash}-${file.originalname}`;
+
+      callback(null, fileName);
+    },
+  }),
 };
