7678e00c6cc327b33320a25664e7c1bde5653c34
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-14 17:42:18 -0300

    Remove verification

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
 import AppError from '@shared/errors/AppError';
-import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import auth from '@config/auth';
 
 interface IPayload {
@@ -24,13 +23,6 @@ export default async function ensureAuthenticated(
 
   try {
     const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
-
-    const usersRepository = new UsersRepository();
-    const user = await usersRepository.findById(user_id);
-
-    if (!user) {
-      throw new AppError('User does not exists', 401);
-    }
 
     request.user = { id: user_id };
 
