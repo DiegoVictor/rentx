4e5ebe390de9abc9485f9542f3999aa67c9b966b
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-15 18:11:25 -0300

    Parse retrieved data

---
+++ src/modules/accounts/mappers/UserMap.ts
@@ -0,0 +1,25 @@
+import { classToClass } from 'class-transformer';
+import IUserResponseDTO from '../dtos/IUserResponseDTO';
+import User from '../infra/typeorm/entities/User';
+
+class UserMap {
+  static toDTO({
+    email,
+    name,
+    id,
+    avatar,
+    driver_license,
+    avatar_url,
+  }: User): IUserResponseDTO {
+    return classToClass({
+      email,
+      name,
+      id,
+      avatar,
+      driver_license,
+      avatar_url,
+    });
+  }
+}
+
+export default UserMap;
