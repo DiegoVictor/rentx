6d82e03de34c8977cea0ce5193a71d0c1605ecc0
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-15 18:13:19 -0300

    Load users routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 
 import categories from './categories';
+import users from './users';
 const app = Router();
 app.use('/categories', categories);
+app.use('/users', users);
 export default app;
