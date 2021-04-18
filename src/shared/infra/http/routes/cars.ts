0a71bc67ce43d227aff6a5dd766917c0b7e62de9
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-18 15:51:30 -0300

    Configure route to create a new car

--- src/shared/infra/http/routes/cars.ts
+++ src/shared/infra/http/routes/cars.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import CreateCarController from '@modules/cars/useCases/createCar/CreateCarController';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import ensureAdmin from '../middlewares/ensureAdmin';
 
 const app = Router();
+const createCarController = new CreateCarController();
+app.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
 
 export default app;
