18d1ed36cbc8bb77ca8107fed332389c0816d15b
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-19 18:45:06 -0300

    Register cars repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,8 @@ import IUsersRepository from '@modules/accounts/repositories/contracts/IUsersRep
 import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/contracts/ICategoriesRepository';
 import CategoriesRepository from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
+import ICarsRepository from '@modules/cars/repositories/contracts/ICarsRepository';
+import CarsRepository from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
   CategoriesRepository
@@ -16,3 +18,5 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
 );
+
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
