275c482f61ea432b656239130a7df52f83b68259
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-19 18:39:06 -0300

    Configure route to get available cars

--- src/shared/infra/http/routes/cars.ts
+++ src/shared/infra/http/routes/cars.ts
@@ -2,9 +2,12 @@ import { Router } from 'express';
 import CreateCarController from '@modules/cars/useCases/createCar/CreateCarController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import ensureAdmin from '../middlewares/ensureAdmin';
+import ListAvailableCarsController from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 
 const app = Router();
+const listAvailableCarsController = new ListAvailableCarsController();
 const createCarController = new CreateCarController();
 app.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
+app.get('/availables', listAvailableCarsController.handle);
 
 export default app;
