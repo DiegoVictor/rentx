e250938fc25c62c91fea0f092a0262e552eaf5e8
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-26 18:09:12 -0300

    Configure route to set car specification

--- src/shared/infra/http/routes/cars.ts
+++ src/shared/infra/http/routes/cars.ts
@@ -3,11 +3,19 @@ import CreateCarController from '@modules/cars/useCases/createCar/CreateCarContr
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import ensureAdmin from '../middlewares/ensureAdmin';
 import ListAvailableCarsController from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
+import CreateCarSpecificationController from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 
 const app = Router();
 const listAvailableCarsController = new ListAvailableCarsController();
-const createCarController = new CreateCarController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 app.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
+app.post(
+  '/:id/specifications',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarSpecificationController.handle
+);
+
 app.get('/availables', listAvailableCarsController.handle);
 
 export default app;
