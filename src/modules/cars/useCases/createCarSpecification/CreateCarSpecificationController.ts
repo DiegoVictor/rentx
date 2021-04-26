3391af2043bcfe7fdc93a74626e007eaad79eece
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-26 18:08:39 -0300

    Create controller to save car's specification

---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CreateCarSpecificationUseCase from './CreateCarSpecificationUseCase';
+
+class CreateCarSpecificationController {
+  async handle(request: Request, response: Response) {
+    const { id } = request.params;
+    const { specifications_id } = request.body;
+    const createCarSpecificationUseCase = container.resolve(
+      CreateCarSpecificationUseCase
+    );
+
+    const car = await createCarSpecificationUseCase.execute({
+      car_id: id,
+      specifications_id,
+    });
+
+    return response.json(car);
+  }
+}
+
+export default CreateCarSpecificationController;
