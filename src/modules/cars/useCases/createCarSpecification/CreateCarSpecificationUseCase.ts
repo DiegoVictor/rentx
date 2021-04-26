77ab23791c92574dd5ce2af03fee2a05d4baddc0
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-26 18:08:12 -0300

    Save car specification

---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -0,0 +1,40 @@
+import { inject, injectable } from 'tsyringe';
+
+import Car from '@modules/cars/infra/typeorm/entities/Car';
+import ICarsRepository from '@modules/cars/repositories/contracts/ICarsRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/contracts/ISpecificationsRepository';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  car_id: string;
+  specifications_id: string[];
+}
+
+@injectable()
+class CreateCarSpecificationUseCase {
+  constructor(
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository,
+
+    @inject('SpecificationsRepository')
+    private specificationsRepository: ISpecificationsRepository
+  ) {}
+
+  async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
+    const car = await this.carsRepository.findById(car_id);
+    if (!car) {
+      throw new AppError('Car does not exists');
+    }
+
+    const specifications = await this.specificationsRepository.findByIds(
+      specifications_id
+    );
+    car.specifications = specifications;
+
+    await this.carsRepository.create(car);
+
+    return car;
+  }
+}
+
+export default CreateCarSpecificationUseCase;
