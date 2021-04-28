e67d54899c7f5e630701613ff29bfc1cfa74ab3f
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-28 20:18:00 -0300

    Create endpoint to upload car image

--- src/shared/infra/http/routes/cars.ts
+++ src/shared/infra/http/routes/cars.ts
@@ -7,6 +7,7 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import ensureAdmin from '../middlewares/ensureAdmin';
 import ListAvailableCarsController from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 import CreateCarSpecificationController from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
+import UploadCarImagesController from '@modules/cars/useCases/uploadCarImages/UploadCarImagesController';
 
 const app = Router();
 const uploadCarImage = multer(uploadConfig);
@@ -14,6 +15,8 @@ const uploadCarImage = multer(uploadConfig);
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
+
 app.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
 app.post(
   '/:id/specifications',
@@ -22,6 +25,13 @@ app.post(
   createCarSpecificationController.handle
 );
 
+app.post(
+  '/:id/images',
+  ensureAuthenticated,
+  ensureAdmin,
+  uploadCarImage.array('car_images'),
+  uploadCarImagesController.handle
+);
 app.get('/availables', listAvailableCarsController.handle);
 
 export default app;
