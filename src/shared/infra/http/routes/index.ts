f9bb10b2388a837c89db20d4f54b2a3fe7c6b011
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-28 20:18:26 -0300

    Import cars routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,7 +2,9 @@ import { Router } from 'express';
 
 import categories from './categories';
 import users from './users';
+import cars from './cars';
 const app = Router();
 app.use('/categories', categories);
 app.use('/users', users);
+app.use('/cars', cars);
 export default app;
