c87f64d9ae2cd934989c3494d1549b3dfa39e56d
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-28 18:48:21 -0300

    Configure specification

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,7 +5,9 @@ import '@shared/container/providers';
 import IUsersRepository from '@modules/accounts/repositories/contracts/IUsersRepository';
 import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/contracts/ICategoriesRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/contracts/ISpecificationsRepository';
 import CategoriesRepository from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
+import SpecificationsRepository from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
 import ICarsRepository from '@modules/cars/repositories/contracts/ICarsRepository';
 import CarsRepository from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 container.registerSingleton<ICategoriesRepository>(
@@ -13,6 +15,10 @@ container.registerSingleton<ICategoriesRepository>(
   CategoriesRepository
 );
 
+container.registerSingleton<ISpecificationsRepository>(
+  'SpecificationsRepository',
+  SpecificationsRepository
+);
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
