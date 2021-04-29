71f4f71d5f29249aae6611f3db2526d55e70b2e3
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-29 19:04:58 -0300

    Create rate limit middleware

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,29 @@
+import redis from 'redis';
+import { NextFunction, Request, Response } from 'express';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+
+import AppError from '@shared/errors/AppError';
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  points: 10,
+  duration: 5,
+});
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  try {
+    await limiter.consume(request.ip);
+    return next();
+  } catch (err) {
+    throw new AppError('Too many requests', 429);
+  }
+}
