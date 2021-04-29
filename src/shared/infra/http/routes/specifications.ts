3d3e3d242ad8929aa70f707e090fe1074c487b65
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-28 21:52:18 -0300

    Create specifications routes

---
+++ src/shared/infra/http/routes/specifications.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
+import CreateSpecificationController from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
+import ensureAdmin from '../middlewares/ensureAdmin';
+
+const app = Router();
+
+const createSpecificationController = new CreateSpecificationController();
+
+app.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationController.handle
+);
+
+export default app;
