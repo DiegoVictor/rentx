45a79d8cf2b9d99e679ce22f3b57edeb1ec3d569
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-29 19:05:16 -0300

    Configure rate limit

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,6 +8,7 @@ import '@shared/container';
 import createConnection from '@shared/infra/typeorm';
 import routes from '@shared/infra/http/routes';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 import upload from '@config/upload';
 
 createConnection();
@@ -17,6 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(rateLimiter);
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
