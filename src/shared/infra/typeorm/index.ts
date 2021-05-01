26882a3129fc8d4625ffc25e8ff7bd58c534c863
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-01 12:31:37 -0300

    Change test database name

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -6,9 +6,7 @@ export default async (): Promise<Connection> => {
   return createConnection(
     Object.assign(defaultOptions, {
       database:
-        process.env.NODE_ENV === 'test'
-          ? 'rentx_test'
-          : defaultOptions.database,
+        process.env.NODE_ENV === 'test' ? 'test' : defaultOptions.database,
     })
   );
 };
