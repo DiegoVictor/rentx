79243fefcb9995c7f36aab4a1351a05eb688dd25
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-02 12:30:29 -0300

    Use torage provider

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import IUsersRepository from '@modules/accounts/repositories/contracts/IUsersRepository';
-import { deleteFile } from '@utils/file';
+import IStorageProvider from '@shared/container/providers/StorageProvider/contracts/IStorageProvider';
 
 interface IRequest {
   user_id: string;
@@ -11,15 +11,20 @@ interface IRequest {
 @injectable()
 class UpdateUserAvatarUseCase {
   constructor(
-    @inject('UsersRepository') private usersRepository: IUsersRepository
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ user_id, avatar_file }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
 
     if (user.avatar) {
-      await deleteFile(`./tmp/avatar/${user.avatar}`);
+      await this.storageProvider.delete(user.avatar, 'avatar');
     }
+    await this.storageProvider.save(avatar_file, 'avatar');
     user.avatar = avatar_file;
 
     await this.usersRepository.create(user);
