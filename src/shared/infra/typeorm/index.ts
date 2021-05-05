04f426cb137eb095eb6f8c3232f92116806ca739
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-05 19:43:07 -0300

    Remove log

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,7 +2,6 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
-  console.log(process.env.NODE_ENV);
 
   return createConnection(
     Object.assign(defaultOptions, {
