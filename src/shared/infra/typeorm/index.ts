643459e923db81842cf1ace5d6c2110aa4c9a517
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-05 19:40:08 -0300

    Show enviroment

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,6 +2,7 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
+  console.log(process.env.NODE_ENV);
 
   return createConnection(
     Object.assign(defaultOptions, {
