854e3eb7b4283909763b3f302b2184e4803d75f0
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-09 15:25:16 -0300

    Allow to get new tokens from the refresh token

--- src/shared/infra/http/routes/authenticate.ts
+++ src/shared/infra/http/routes/authenticate.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 
 import AuthenticateUserController from '@modules/accounts/useCases/authenticateUser/AuthenticateUserController';
+import RefreshTokenController from '@modules/accounts/useCases/refreshToken/RefreshTokenController';
+
 const app = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
+
 app.post('/sessions', authenticateUserController.handle);
+app.post('/refresh_token', refreshTokenController.handle);
+
 export default app;
