ebddab633effc5379e7c06bcea39bf84bdd65d09
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-10 19:47:46 -0300

    Register rental creation route

--- src/shared/infra/http/routes/rentals.ts
+++ src/shared/infra/http/routes/rentals.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express';
 
+import CreateRentalController from '@modules/rentals/useCases/createRental/CreateRentalController';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const app = Router();
+
+const createRentalController = new CreateRentalController();
+app.post('/', ensureAuthenticated, createRentalController.handle);
 
 export default app;
