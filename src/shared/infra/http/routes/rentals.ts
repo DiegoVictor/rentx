347c1436749fdbff9871db5fba85301504c51c96
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-11 17:36:04 -0300

    Configure endpoint to retrieve rentals

--- src/shared/infra/http/routes/rentals.ts
+++ src/shared/infra/http/routes/rentals.ts
@@ -2,10 +2,14 @@ import { Router } from 'express';
 
 import CreateRentalController from '@modules/rentals/useCases/createRental/CreateRentalController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import ListRentalsByUserController from '@modules/rentals/useCases/listByUser/ListRentalsByUserController';
 
 const app = Router();
 
+const listRentalsByUserController = new ListRentalsByUserController();
 const createRentalController = new CreateRentalController();
+
+app.get('/user', ensureAuthenticated, listRentalsByUserController.handle);
 app.post('/', ensureAuthenticated, createRentalController.handle);
 
 export default app;
