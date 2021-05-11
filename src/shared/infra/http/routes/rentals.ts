93da22281150a654dda82b19491d32c1d0ab51f9
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-11 17:38:31 -0300

    Configure route to car devolution

--- src/shared/infra/http/routes/rentals.ts
+++ src/shared/infra/http/routes/rentals.ts
@@ -2,14 +2,21 @@ import { Router } from 'express';
 
 import CreateRentalController from '@modules/rentals/useCases/createRental/CreateRentalController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import DevolutionRentalController from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
 import ListRentalsByUserController from '@modules/rentals/useCases/listByUser/ListRentalsByUserController';
 
 const app = Router();
 
 const listRentalsByUserController = new ListRentalsByUserController();
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 app.get('/user', ensureAuthenticated, listRentalsByUserController.handle);
 app.post('/', ensureAuthenticated, createRentalController.handle);
+app.post(
+  '/:id/devolution',
+  ensureAuthenticated,
+  devolutionRentalController.handle
+);
 
 export default app;
