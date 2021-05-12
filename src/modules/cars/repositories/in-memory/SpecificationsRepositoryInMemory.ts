a7d81f7f5c18e901e0d564ec938bdd3c0f9989ef
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-12 18:42:51 -0300

    Create specification repository in memory

---
+++ src/modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory.ts
@@ -0,0 +1,35 @@
+import Specification from '@modules/cars/infra/typeorm/entities/Specification';
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from '../contracts/ISpecificationsRepository';
+
+class SpecificationsRepositoryInMemory implements ISpecificationsRepository {
+  specifications: Specification[] = [];
+
+  async create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification> {
+    const specification = new Specification();
+
+    Object.assign(specification, { name, description });
+
+    this.specifications.push(specification);
+    return specification;
+  }
+
+  async findByName(name: string): Promise<Specification> {
+    return this.specifications.find(
+      (specification) => specification.name === name
+    );
+  }
+
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    return this.specifications.filter((specification) =>
+      ids.includes(specification.id)
+    );
+  }
+}
+
+export default SpecificationsRepositoryInMemory;
