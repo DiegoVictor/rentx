8cf294e2bf15b2f75706a18d5b6e2ade0d4fe527
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-13 20:09:23 -0300

    Create route to start reset password process

--- src/shared/infra/http/routes/password.ts
+++ src/shared/infra/http/routes/password.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
+import SendForgotPasswordMailController from '@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController';
 const app = Router();
+
+const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+app.post('/forgot', sendForgotPasswordMailController.handle);
 
 export default app;
