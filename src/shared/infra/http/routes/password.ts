e41a4bf5a88fcc0df7cdbe72afc8f0c282b6c224
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-13 20:10:04 -0300

    Create route to set the new password

--- src/shared/infra/http/routes/password.ts
+++ src/shared/infra/http/routes/password.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import SendForgotPasswordMailController from '@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController';
+import ResetUserPasswordController from '@modules/accounts/useCases/resetUserPassword/ResetUserPasswordController';
 const app = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
-app.post('/forgot', sendForgotPasswordMailController.handle);
+const resetUserPasswordController = new ResetUserPasswordController();
+
+app.post('/reset', resetUserPasswordController.handle);
 
 export default app;
