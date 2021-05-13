e5264e62786245cc1a75d3d2154283cd2bf1dce2
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-13 20:04:39 -0300

    Return the connection

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,11 +2,16 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
+  let connection: Connection;
 
-  return createConnection(
-    Object.assign(defaultOptions, {
-      database:
-        process.env.NODE_ENV === 'test' ? 'test' : defaultOptions.database,
-    })
-  );
+  if (!connection) {
+    connection = await createConnection(
+      Object.assign(defaultOptions, {
+        database:
+          process.env.NODE_ENV === 'test' ? 'test' : defaultOptions.database,
+      })
+    );
+  }
+
+  return connection;
 };
