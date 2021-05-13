f66c174e36a529cce8d16277c5dfea8a36eefbd6
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-13 20:07:07 -0300

    Update repository contract

--- src/modules/accounts/repositories/contracts/IUsersRepository.ts
+++ src/modules/accounts/repositories/contracts/IUsersRepository.ts
@@ -2,7 +2,7 @@ import ICreateUserDTO from '@modules/accounts/dtos/ICreateUserDTO';
 import User from '@modules/accounts/infra/typeorm/entities/User';
 
 interface IUsersRepository {
-  create(data: ICreateUserDTO): Promise<void>;
+  create(data: ICreateUserDTO): Promise<User>;
   findByEmail(email: string): Promise<User>;
   findById(id: string): Promise<User>;
 }
