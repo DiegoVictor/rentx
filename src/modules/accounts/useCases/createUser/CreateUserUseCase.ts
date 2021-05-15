db59aa448e3373764d51893838643bd666670b80
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-15 15:37:50 -0300

    Parse user before return

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -4,6 +4,8 @@ import { inject, injectable } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/accounts/repositories/contracts/IUsersRepository';
 import ICreateUserDTO from '@modules/accounts/dtos/ICreateUserDTO';
+import UserMap from '@modules/accounts/mappers/UserMap';
+import IUserResponseDTO from '@modules/accounts/dtos/IUserResponseDTO';
 
 @injectable()
 class CreateUserUseCase {
@@ -17,20 +19,21 @@ class CreateUserUseCase {
     email,
     driver_license,
     password,
-  }: ICreateUserDTO): Promise<void> {
+  }: ICreateUserDTO): Promise<IUserResponseDTO> {
     const hashedPassword = await hash(password, 8);
 
-    const user = await this.usersRepository.findByEmail(email);
-    if (user) {
+    if (await this.usersRepository.findByEmail(email)) {
       throw new AppError('User already exists', 240);
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       driver_license,
       password: hashedPassword,
     });
+
+    return UserMap.toDTO(user);
   }
 }
 
