cd6594ac82429d41b3982e79dda463752c0a0e87
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-15 15:38:15 -0300

    Throw error when user not exists

--- src/modules/accounts/useCases/profileUser/ProfileUserUseCase.ts
+++ src/modules/accounts/useCases/profileUser/ProfileUserUseCase.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from 'tsyringe';
 import IUsersRepository from '@modules/accounts/repositories/contracts/IUsersRepository';
 import IUserResponseDTO from '@modules/accounts/dtos/IUserResponseDTO';
 import UserMap from '@modules/accounts/mappers/UserMap';
+import AppError from '@shared/errors/AppError';
 
 @injectable()
 class ProfileUserUseCase {
@@ -13,6 +14,10 @@ class ProfileUserUseCase {
 
   async execute(id: string): Promise<IUserResponseDTO> {
     const user = await this.usersRepository.findById(id);
+
+    if (!user) {
+      throw new AppError('User does not exists', 245);
+    }
 
     return UserMap.toDTO(user);
   }
