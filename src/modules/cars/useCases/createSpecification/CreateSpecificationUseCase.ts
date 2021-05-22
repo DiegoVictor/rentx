5945758dfb25aca8223b9409dee8bc0d9ad161f3
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-22 00:26:50 -0300

    Return the created specification

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import { ISpecificationsRepository } from '@modules/cars/repositories/contracts/ISpecificationsRepository';
+import Specification from '@modules/cars/infra/typeorm/entities/Specification';
 
 interface IRequest {
   name: string;
@@ -15,12 +16,16 @@ class CreateSpecificationUseCase {
     private specificationRepository: ISpecificationsRepository
   ) {}
 
-  async execute({ name, description }: IRequest): Promise<void> {
+  async execute({ name, description }: IRequest): Promise<Specification> {
     if (await this.specificationRepository.findByName(name)) {
       throw new AppError('Specification already exists', 540);
     }
 
-    await this.specificationRepository.create({ name, description });
+    const specification = await this.specificationRepository.create({
+      name,
+      description,
+    });
+    return specification;
   }
 }
 
