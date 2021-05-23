8e507a1fb986f3bac7ef3be4c852df5ce671dea8
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-05-23 12:21:20 -0300

    Create user factory

--- tests/utils/factory.ts
+++ tests/utils/factory.ts
@@ -1,4 +1,14 @@
 import faker from 'faker';
 import factory from 'factory-girl';
 
+factory.define(
+  'User',
+  {},
+  {
+    name: faker.name.findName,
+    password: faker.internet.password,
+    driver_license: () => faker.random.alphaNumeric(11),
+    email: faker.internet.email,
+  }
+);
 export default factory;
